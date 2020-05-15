d39b3d3f9407e5d2edce48928148dd83920584d3
masoudNemati <masoud.nnr@gmail.com>
2020-05-15 13:35:55 +0430

    added form that you can write and state will be change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { Component } from "react";
+import Header from "./components/layouts/Header";
 import Todos from "./components/Todos";
-import { Component } from "react";
+import AddTodo from "./components/AddTodo";
 import "./App.css";
 
 class App extends Component {
@@ -46,11 +47,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Todos
-          todos={this.state.todos}
-          markComplete={this.markComplete}
-          delTodo={this.delTodo}
-        />
+        <div className="container">
+          <Header />
+          <AddTodo />
+          <Todos
+            todos={this.state.todos}
+            markComplete={this.markComplete}
+            delTodo={this.delTodo}
+          />
+        </div>
       </div>
     );
   }
--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,5 +1,16 @@
 import React from "react";
 
 export default function Header() {
-  return <div></div>;
+  return (
+    <header style={headerStyle}>
+      <h1>To Do List</h1>
+    </header>
+  );
 }
+
+const headerStyle = {
+  background: "#333",
+  color: "#fff",
+  textAlign: "center",
+  padding: "10px",
+};
